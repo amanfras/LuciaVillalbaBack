968e14efda580dc8c92d01c07947993e9853f582
Amanfras <adela.97mf@gmail.com>
2021-12-20 23:02:56 +0100

    prueba img

--- app.py
+++ app.py
@@ -5,16 +5,23 @@ Created on Sun Dec 19 15:48:08 2021
 @author: Adela
 """
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, flash, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 import os
+import urllib.request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static/uploads/'
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -38,6 +45,27 @@ class UserSchema(ma.Schema):
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    imagen = db.Column(db.String, nullable=False)
+    fecha = db.Column(db.String(), nullable=False)
+
+
+    def __init__(self, title, content, fecha, imagen):
+        self.title = title
+        self.content = content
+        self.fecha=fecha
+        self.imagen=imagen
+
+class BlogSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "content", "fecha", "imagen")
+
+blog_schema = BlogSchema()
+blogs_schema = BlogSchema(many=True)
+
 
 @app.route("/user/add", methods=["POST"])
 def add_user():
@@ -54,6 +82,55 @@ def add_user():
 def get_all_users():
     all_users = User.query.all()
     return jsonify(users_schema.dump(all_users))
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route("/blog/add", methods=["POST"])
+def add_blog():
+    title = request.json.get("title")
+    content = request.json.get("content")
+    fecha = request.json.get("fecha")
+    imagen = request.json.get("imagen")
+
+    record = Blog(title, content, fecha, imagen)
+    db.session.add(record)
+    db.session.commit()
+
+    return jsonify(blog_schema.dump(record))
+
+@app.route("/blog/delete/<id>", methods=["DELETE"])
+def delete_blog(id):
+    record = Blog.query.get(id)
+    db.session.delete(record)
+    db.session.commit()
+
+    return jsonify(blog_schema.dump(record))
+
+@app.route("/blog/blog/<id>", methods=["PUT"])
+def update_blog(id):
+    record = Blog.query.get(id)
+    title = request.json.get("title")
+    content = request.json.get("content")
+    fecha = request.json.get("fecha")
+    imagen = request.json.get("imagen")
+    
+    record.title=title
+    record.content=content
+    record.fecha=fecha
+    record.imagen=imagen
+    
+    db.session.commit()
+
+    return jsonify(blog_schema.dump(record))
+
+@app.route("/blog/get", methods=["GET"])
+def get_all_blogs():
+    all_blogs = Blog.query.all()
+    return jsonify(blogs_schema.dump(all_blogs))
 
 
 if __name__ == "__main__":
